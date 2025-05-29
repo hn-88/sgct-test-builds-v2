00d40357c5afa821a7741b2f7700524426e3d60a
hn-88 <hari@sssmediacentre.org>
2025-05-29 13:40:51 +0530

    some parenthesis close were missed in image.cpp with Err()

--- src/image.cpp
+++ src/image.cpp
@@ -80,7 +80,7 @@ void Image::load(const std::filesystem::path& filename) {
     _data = stbi_load(name.c_str(), &_size.x, &_size.y, &_nChannels, 0);
     if (_data == nullptr) {
         throw Err(
-            9001, std::format("Could not open file '{}' for loading image", filename.string());
+            9001, std::format("Could not open file '{}' for loading image", filename.string()));
     }
     _bytesPerChannel = 1;
     _dataSize = _size.x * _size.y * _nChannels * _bytesPerChannel;
@@ -127,7 +127,7 @@ void Image::save(const std::filesystem::path& filename) {
     std::string f = filename.string();
     FILE* fp = fopen(f.c_str(), "wb");
     if (fp == nullptr) {
-        throw Err(9008, std::format("Cannot create PNG file '{}'", filename.string());
+        throw Err(9008, std::format("Cannot create PNG file '{}'", filename.string()));
     }
 
     // initialize stuff
